55835043eb1e2fab478862b0b0f9aa30c1fa1e30
codercreighton <codercreighton@gmail.com>
2018-05-16 11:28:19 -0400

    added shortened links

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,7 +5,51 @@ class Tweet < ApplicationRecord
 	has_many :tweet_tags
 	has_many :tags, through: :tweet_tags
 
-	validates :message, presence: true, length: {maximum: 140, too_long: "A tweet is 140 characters.  Maybe you want Facebook?"}
+	before_validation :link_check, on: :create
+	validates :message, presence: true, length: {maximum: 140, too_long: "A tweet is 140 characters.  Maybe you want Facebook?"}, on: :create
 
+	after_validation :apply_link, on: :create
+
+
+
+	private
+
+	  def link_check
+	    check = false
+	    if self.message.include?("http://") || self.message.include?("https://") || self.message.include?("www.")
+				check = true
+			end	
+	  
+	    if check == true
+	    	arr = self.message.split
+	    	index = arr.map{ |x| x.include? "http"}.index(true)
+	      self.link = arr[index]
+
+
+	      puts "----------------"
+	      puts arr[index]
+	      puts "-----------------"
+
+	      if arr[index].length > 23
+	  	    arr[index] = "#{arr[index][0..20]}..."	
+	      end
+	  				
+	      self.message = arr.join(" ")
+	    end
+	  end	
+
+	  def apply_link
+	  	arr = self.message.split
+
+	  	index = arr.map{|x| x.include?("http://") || x.include?("https://") || x.include?("www.")}.index(true)
+
+	  	if index
+	  		url = arr[index]
+	  		arr[index] = "<a href='#{self.link}' target='_blank' > #{url}</a>"
+	  	end
+	  	
+	  	self.message = arr.join(" ")	
+
+	  end	
 
 end
